6f4a30b919e0e6435efff1242c2a209d6459ddef
Бекзод <bek9669@bk.ru>
2024-09-21 10:33:04 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+name = 'Бекзод'
+print('Имя: ' + name)
+age = 28
+print('Возраст: ' + str(age))
+age = 28 + 1
+print('Новый возраст: ' + str(age))
+print('Новый возраст: ' + str(age + 1))
+is_student = True
+print('Статус студента: ' + str(is_student))
