227281fba834b3b28f2546f76c74f156882e8b9c
Бекзод <bek9669@bk.ru>
2024-09-21 15:23:13 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list= ['apple', 'apricot', 'avocado', 'banana', 'kiwi']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:4])
+my_list[2]= 'orange'
+print(my_list)
+my_dict= {'work': 'работа', 'home': 'дом', 'office': 'офис'}
+print(my_dict)
+print(my_dict['work'])
+my_dict['home']= 'домой'
+my_dict['at home']= 'дома'
+print(my_dict)
